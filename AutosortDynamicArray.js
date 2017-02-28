a44214e112eaef1fc56b15e824634a10aec5bd2b
DigitalWhale <jargiv@ya.ru>
2017-02-28 21:07:24 +0300

    The basic version

---
+++ AutosortDynamicArray.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let AArray = () => {
+  let array  = []
+};
+
+//Сортировка элемента
+AArray.prototype.sortDown = () => {
+
+};
+
+//Добавление элемента
+AArray.prototype.push = (element) => {
+
+};
