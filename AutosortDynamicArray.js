9da02eadd4079d6b51f6c68502deda52e6168266
DigitalWhale <jargiv@ya.ru>
2017-03-01 00:43:36 +0300

    Fixed several bug. Added exaple

--- AutosortDynamicArray.js
+++ AutosortDynamicArray.js
@@ -1,26 +1,25 @@
 'use strict';
 
-let AArray = (typeSort) => { //typeSort: true - сортирвка по возрастанию, false - по убыванию
+function AArray(typeSort){ //typeSort: true - сортирвка по возрастанию, false - по убыванию
   this.array  = [];
-  try {
-      if (typeof (typeSort) == "function") {
-          this.typeSort = typeSort;
-      }
-      else {
-          this.typeSort = (a, b) => {
-              if(a.toString() > b.toString()){
-                  return true;
-              }
-              else {
-                  return false;
-              }
-          };
-      }
-  }
+   if (typeof (typeSort) == "function") {
+       this.typeSort = typeSort;
+   }
+   else {
+       this.typeSort = function(a, b){
+           if(a.toString() > b.toString()){
+               return true;
+           }
+           else {
+               return false;
+           }
+       };
+   }
+
 };
 
 //Сортировка элемента
-AArray.prototype.sortInvert = (index) => {
+AArray.prototype.sortInvert = function(index){
     while(index != 0 && !this.typeSort(this.array[index], this.array[index-1])){
         let buffer = this.array[index];
         this.array[index] = this.array[index-1];
@@ -29,8 +28,8 @@ AArray.prototype.sortInvert = (index) => {
     }
 };
 
-AArray.prototype.sort = (index) => {
-    while(index != 0 && this.typeSort()){
+AArray.prototype.sort = function(index){
+    while(index != this.array.length-1 && this.typeSort(this.array[index], this.array[index+1])){
         let buffer = this.array[index];
         this.array[index] = this.array[index+1];
         this.array[index+1]=buffer;
@@ -39,13 +38,18 @@ AArray.prototype.sort = (index) => {
 };
 
 //Добавление элемента
-AArray.prototype.push = (element) => {
+AArray.prototype.push = function(element){
     this.array.push(element);
     this.sortInvert(this.array.length-1);
 };
 
-AArray.prototype.edit = (index, value) => {
-  this.array[index] = value;
+AArray.prototype.edit = function(index, value, pole){
+    if(pole === null) {
+        this.array[index] = value;
+    }
+    else {
+        this.array[index][pole] = value;
+    }
   if(this.typeSort(this.array[index], this.array[index+1])){
       this.sort(index);
   } else  if(this.typeSort(this.array[index-1], this.array[index])){
@@ -53,37 +57,19 @@ AArray.prototype.edit = (index, value) => {
   }
 };
 
-AArray.prototype.increment = (index) => {
-    this.array[index]++;
-    if(this.typeSort(this.array[index], this.array[index+1])){
-        this.sort(index);
-    } else  if(this.typeSort(this.array[index-1], this.array[index])){
-        this.sortInvert(index);
-    }
-};
-
-AArray.prototype.decrement = (index) => {
-    this.array[index]--;
-    if(this.typeSort(this.array[index], this.array[index+1])){
-        this.sort(index);
-    } else  if(this.typeSort(this.array[index-1], this.array[index])){
-        this.sortInvert(index);
-    }
-};
-
-AArray.prototype.get = (index) => {
+AArray.prototype.get = function(index){
     return this.array[index];
 };
 
-AArray.prototype.getArr = () => {
+AArray.prototype.getArr = function(){
     return this.array;
 };
 
-AArray.prototype.size = () => {
+AArray.prototype.size = function(){
     return this.array.length;
 };
 
-AArray.prototype.clear = () => {
+AArray.prototype.clear = function(){
     this.arr = [];
 };
 
---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let arr = [{name: "lol1", val: 1}, {name: "lol2", val: 4}, {name: "lol3", val: 3}, {name: "lol4", val: 1}, {name: "lol5", val: 6}, {name: "lol6", val: 4}, {name: "lol7", val: 1}, {name: "lol8", val: 8}, {name: "lol9", val: 10}];
+let darr = new AArray((a, b) => {
+    if(a["val"] > b["val"]){
+        return true;
+    }
+    else {
+        return false;
+    }
+});
+for(let i = 0; i < arr.length; i++){
+    darr.push(arr[i]);
+}
+for(let i = 0; i <  darr.size(); i++){
+    console.log(darr.get(i))
+}
+
+console.log("incr");
+
+darr.edit(6, 1 , "val");
+
+for(let i = 0; i <  darr.size(); i++){
+    console.log(darr.get(i))
+}
+
